267af98e375508f080e5f7f5baa9c016be4b87b5
Naveen-g09 <naveengummella.rg@gmail.com>
2024-05-16 21:52:54 +0530

    modules

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,13 +4,28 @@ export const singupInput = z.object({
     username: z.string(),
     password: z.string().min(6),
     name: z.string().optional(),
-    });
+});
 
 export type SignupInput = z.infer<typeof singupInput>;
 
 export const singInInput = z.object({
     username: z.string(),
     password: z.string().min(6)
-    });
+});
 
 export type SignInInput = z.infer<typeof singupInput>;
+
+export const createBlogInput = z.object({
+    title: z.string(),
+    content: z.string()
+});
+
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+
+export const UpdateBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.number()
+});
+
+export type UpdateBlogInput = z.infer<typeof UpdateBlogInput>;
