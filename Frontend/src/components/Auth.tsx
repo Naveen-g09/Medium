af64c956bc1d7fd9d5e7ccf984792422a7b785eb
Naveen-g09 <naveengummella.rg@gmail.com>
2024-05-19 00:18:03 +0530

    auth

--- Frontend/src/components/Auth.tsx
+++ Frontend/src/components/Auth.tsx
@@ -1,27 +1,66 @@
-import { useState } from "react";
-import { Link } from "react-router-dom"
-import { SignupInput, singupInput } from "@naveen-g09/zod-medium-types";
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SignupInput } from "@naveen-g09/zod-medium-types";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
-    const postInputs = useState<SignupInput>({
+    const navigate = useNavigate();
+    const [postInputs, setPostInputs] = useState<SignupInput>({
         name: "",
         username: "",
         password: ""
-    })[0]
+    });
 
-    return (
-        <div className="h-screen flex justify-center flex-col">
-            <div className="flex justify-center">
-                <div className="text-3xl font-extrabold">
-                    Create an Account
+    async function sendRequest() {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs");
+        } catch(e) {
+            alert("Error while signing up");
+        }
+    }
+    
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+                <div className="px-10">
+                    <div className="text-3xl font-extrabold">
+                        Create an account
+                    </div>
+                    <div className="text-slate-500">
+                        {type === "signin" ? "Don't have an account?" : "Already have an account?" }
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                            {type === "signin" ? "Sign up" : "Sign in"}
+                        </Link>
+                    </div>
                 </div>
-                <div className="text-slate-400">
-                    Already have an account?
-                    <Link className="pl-2 underline" to={"/signin"}>Login</Link>
+                <div className="pt-8">
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Naveen Gummella..." onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} /> : null}
+                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            username: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            password: e.target.value
+                        })
+                    }} />
+                    <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
         </div>
-    )
+    </div>
 }
 
 interface LabelledInputType {
